3ce6f45ddf6c96258b09568ce8d4d101e5c79eea
Miguel Otero <otemi89@gmail.com>
2015-01-23 13:45:27 +0100

    Add indicators with some dummy data to Mongo DB

--- parsing/excel2dom.py
+++ parsing/excel2dom.py
@@ -1,1 +1,11 @@
+from webindex.domain.model.indicator.indicator import *
+
 __author__ = 'Miguel'
+
+
+class Excel2Dom(object):
+
+    @staticmethod
+    def excel_indicator_to_dom(excel_indicator):
+        indicator = create_indicator(_type=excel_indicator.type, label=excel_indicator.name, code=excel_indicator.code)
+        return indicator
--- parsing/parser.py
+++ parsing/parser.py
@@ -1,5 +1,7 @@
 import xlrd
 from application.a4aiFetcher.parsing.excel_model.excel_indicator import ExcelIndicator
+from infrastructure.mongo_repos.indicator_repository import IndicatorRepository
+from application.a4aiFetcher.parsing.excel2dom import Excel2Dom
 
 __author__ = 'Miguel'
 
@@ -9,13 +11,14 @@ class Parser(object):
     def __init__(self, log, config):
         self._log = log
         self._config = config
-        self._indicators = []
+        self._excel_indicators = []
 
     def run(self):
         self._log.info("Running parser")
         indicator_sheet, data_sheet = self.initialize_sheets()
+        indicator_repo = self.initialize_repositories()
         self.retrieve_indicators(indicator_sheet)
-        self.store_indicators()
+        self.store_indicators(indicator_repo)
         self.retrieve_data(data_sheet)
         self._log.info("Parsing finished")
 
@@ -29,6 +32,10 @@ class Parser(object):
         indicator_sheet = self.get_sheet(structure_file_name, indicator_sheet_number)
         data_sheet = self.get_sheet(data_file_name, data_sheet_number)
         return indicator_sheet, data_sheet
+
+    def initialize_repositories(self):
+        indicator_repo = IndicatorRepository(self._config.get("CONNECTION", "MONGO_IP"))
+        return indicator_repo
 
     @staticmethod
     def get_sheet(file_name, sheet_number):
@@ -49,16 +56,19 @@ class Parser(object):
             _type = indicator_sheet.cell(row_number, type_column).value
             subindex_code = indicator_sheet.cell(row_number, subindex_column).value
             indicator = ExcelIndicator(code, name, _type, subindex_code)
-            self._indicators.append(indicator)
+            self._excel_indicators.append(indicator)
             print indicator.code
 
-    def store_indicators(self):
-        pass
+    def store_indicators(self, indicator_repo):
+        for excel_indicator in self._excel_indicators:
+            indicator = Excel2Dom.excel_indicator_to_dom(excel_indicator)
+            indicator_repo.insert_indicator(indicator, indicator_uri="EJEMPLO_URI", index_name="INDEX")
 
     def retrieve_data(self, data_sheet):
         countries_column = self._config.getint("DATA_ACCESS", "COUNTRIES_COLUMN")
         indicator_columns_range = self._config.get("DATA_ACCESS", "INDICATOR_COLUMNS_RANGE").split(", ")
         indicator_names_row = self._config.getint("DATA_ACCESS", "INDICATOR_NAMES_ROW")
+
         for row_number in range(1, data_sheet.nrows):
             country_name = data_sheet.cell(row_number, countries_column).value
             for column_number in range(int(indicator_columns_range[0]), int(indicator_columns_range[1]) + 1):
