c7b886e315462e771e826c1ce0b5a90666ec7a20
Miguel Otero <otemi89@gmail.com>
2015-02-02 21:32:20 +0100

    Retrieve rankings for grouped observations

--- parsing/grouped_observation_parser.py
+++ parsing/grouped_observation_parser.py
@@ -25,25 +25,29 @@ class GroupedObservationParser(Parser):
         return index_subindex_sheet
 
     def retrieve_grouped_observations(self, grouped_obs_sheet):
-        countries_column = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "COUNTRIES_COLUMN")
-        indicator_columns_range = self._config.get("INDEX_SUBINDEX_OBSERVATIONS", "INDICATOR_COLUMNS_RANGE").split(", ")
+        country_column = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "COUNTRY_COLUMN")
+        indicator_column = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "INDICATOR_COLUMN")
+        ranking_column = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "RANKING_COLUMN")
         indicator_names_row = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "INDICATOR_NAMES_ROW")
-        ranking_column = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "OVERALL_RANKING_COLUMN")
+        indicator_quantity = self._config.getint("INDEX_SUBINDEX_OBSERVATIONS", "INDICATOR_QUANTITY")
 
         for row_number in range(1, grouped_obs_sheet.nrows):
-            country_name = grouped_obs_sheet.cell(row_number, countries_column).value
-            for column_number in range(int(indicator_columns_range[0]), int(indicator_columns_range[1]) + 1):
-                retrieved_indicator_code = grouped_obs_sheet.cell(indicator_names_row, column_number).value
+            column_offset = 0
+            for indicator_num in range(0, indicator_quantity):
+                country_name = grouped_obs_sheet.cell(row_number, country_column + column_offset).value
+                retrieved_indicator_code = grouped_obs_sheet.cell(indicator_names_row,
+                                                                  indicator_column + column_offset).value
                 indicator_code = retrieved_indicator_code.upper()
-                observation_value = grouped_obs_sheet.cell(row_number, column_number).value
-                index_overall_ranking = None
-                if indicator_code == "INDEX":
-                    index_overall_ranking = int(grouped_obs_sheet.cell(row_number, ranking_column).value)
-                observation = ExcelObservation(country_name, indicator_code, observation_value, index_overall_ranking)
+                ranking = grouped_obs_sheet.cell(row_number, ranking_column + column_offset).value
+                observation_value = grouped_obs_sheet.cell(row_number, indicator_column + column_offset).value
+
+                observation = ExcelObservation(country_name, indicator_code, observation_value, ranking=ranking)
                 self._excel_grouped_observations.append(observation)
+                column_offset += 4
 
     def store_grouped_observations(self, observation_repo, indicator_repo, area_repo):
         for excel_observation in self._excel_grouped_observations:
+            print excel_observation.country_name
             area = area_repo.find_by_name(excel_observation.country_name)
             indicator = indicator_repo.find_indicators_by_code(excel_observation.indicator_code)
             observation = excel_observation_to_dom(excel_observation, area, indicator)
@@ -55,4 +59,4 @@ class GroupedObservationParser(Parser):
                                                 indicator_name=indicator.name, republish=indicator.republish,
                                                 area_code=area.area, provider_name=indicator.provider_name,
                                                 provider_url=indicator.provider_url, short_name=area.short_name,
-                                                area_type=area.type)
+                                                area_type=area.type, ranking=excel_observation.ranking)
